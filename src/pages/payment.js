9770a8fa9d038915c60935c10d2022472ec9ae14
Aman Singh <singhaman82136@gmail.com>
2023-04-26 16:52:54 +0530

    initial commit

--- src/pages/payment.js
+++ src/pages/payment.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import Cart from '../components/Brand/Cart/Cart'
+//import Cart from '../components/Brand/Cart/Cart'
+import StripeCheckout from 'react-stripe-checkout';
+
 
 function Payment() {
+
+    const onToken = (token) => {
+        console.log(token)
+    }
+
   return (
   
-    <div>
-     <Cart />
-      This is the payment page
-    </div>
+<StripeCheckout
+        token={onToken}
+        stripeKey="pk_test_51N15TsSEGfTGwgMjFl0f8M2pLIHzs40sBQP4yZjBCZczqKHiTHy9go5vUdqpLws1bQUL1ZEhkKygclHBJKWmE3MP00eI2fwIrb"
+      />
+
+    //<div>
+    // <Cart />
+    //  This is the payment page
+    //</div>
   )
 }
 
